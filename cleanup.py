011572b9689b7968b632b874d9d0bdae0c624688
Ilija Vukotic <ivukotic@cern.ch>
2015-01-28 10:26:50 -0800

    added python xml parsing

--- cleanup.py
+++ cleanup.py
@@ -2,15 +2,23 @@
 from datetime import datetime
 import xml.etree.ElementTree as ET
 
-@outputSchema("CLEANED:tuple(SRC:chararray,SITE:chararray,TOS:long,TOD:long,IN:long,OUT:long)")
+@outputSchema("CLEANED:tuple(SRC:chararray,SITE:chararray,TOS:long,TOD:long,TOE:long,IN:long,OUT:long)")
 def XMLtoNTUP(xmlInput):
     ntup = []
     root = ET.fromstring(xmlInput)
-    print root.attrib
+    SRC=root.attrib['src']   # server name
+    SITE=root.attrib['site'] # sitename as set by the endpoint
+    TOS=root.attrib['tos']   # time the service started
+    TOD=root.attrib['tod']   # time the statistics gathering started
+    
     for child in root:
         print child.tag, child.attrib
         if child.attrib['id']=='link': 
             for c in child:
-                if c.tag=='in': r_in=long(c.text)
-                if c.tag=='out': r_out=long(c.text)
-    return (root.attrib['src'],root.attrib['site'],root.attrib['tos'],root.attrib['tod'],r_in,r_out)
+                if c.tag=='in': IN=long(c.text)
+                if c.tag=='out': OUT=long(c.text)
+                
+        if child.attrib['id']=='sgen':
+            for c in child:
+                if c.tag=='toe': TOE=long(c.text)
+    return (SRC,SITE,TOS,TOD,TOE,IN,OUT)
