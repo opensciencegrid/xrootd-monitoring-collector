c9deafef21ee20c914c96db7f4edb39240a7af8e
Ilija Vukotic <ivukotic@cern.ch>
2015-02-16 15:36:47 -0600

    sc

--- startDigest.sh
+++ startDigest.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 DateToProcess=$(date --date="1 days ago" +%Y-%m-%d)
 
@@ -7,4 +8,12 @@ echo "Cleaning new data."
 pig -f cleaner.pig -param INPF=${DateToProcess} 
 
 echo "Grouping ... "
+
+if hadoop fs -test –d Summary/Maxis ; then
+    echo "Maxis directory exists"
+else
+    hadoop fs -mkdir Summary/Maxis
+    echo “Creating Maxis directory”
+fi
+
 pig -f grouper.pig -param INPF=${DateToProcess} 
