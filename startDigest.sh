84ae9c0de93aeba8da018b051f3d1ac53dce6472
Ilija Vukotic <ivukotic@cern.ch>
2015-02-17 15:29:07 -0600

    grouper.

--- startDigest.sh
+++ startDigest.sh
@@ -9,20 +9,5 @@ pig -f cleaner.pig -param INPF=${DateToProcess}
 
 echo "Grouping ... "
 
-if hdfs dfs -test -d Summary/Maxis ; then
-    echo "Maxis directory exists"
-else
-    hdfs dfs -mkdir Summary/Maxis
-    echo "Creating Maxis directory"
-fi
 
 pig -f grouper.pig -param INPF=${DateToProcess} 
-
-echo "removing old last values."
-hdfs dfs -rm -R /user/ivukotic/Summary/MaxisOLD
-
-echo "moving current last values to OLD."
-hdfs dfs -mv '/user/ivukotic/Summary/Maxis' '/user/ivukotic/Summary/MaxisOLD';
-
-echo "moving new last values to current."
-hdfs dfs -mv '/user/ivukotic/Summary/MaxisNEW' '/user/ivukotic/Summary/Maxis';
