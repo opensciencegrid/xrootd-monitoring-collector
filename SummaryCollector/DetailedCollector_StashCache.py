d98effb9f929e1f542f21133f7c39602f1e6696a
Derek Weitzel <dweitzel@cse.unl.edu>
2018-03-14 20:46:27 +0000

    Many changes to the StashCache collector

--- SummaryCollector/DetailedCollector_StashCache.py
+++ SummaryCollector/DetailedCollector_StashCache.py
@@ -16,6 +16,9 @@ from elasticsearch import helpers
 import logging
 import logging.config
 
+import ConfigParser
+import pika
+
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('DetailedCollector')
 
@@ -26,7 +29,8 @@ hostIP=socket.gethostbyname(socket.gethostname())
 DETAILED_PORT = 9930
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-sock.bind((hostIP, DETAILED_PORT))
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,1024*1024*4) 
+sock.bind(("0.0.0.0", DETAILED_PORT))
 
 def RefreshConnection():
     global es
@@ -35,26 +39,40 @@ def RefreshConnection():
         return
     lastReconnectionTime=time.time()
     logger.info('make sure we are connected right...')
-    res = requests.get('http://atlas-kibana.mwt2.org:9200')
-    logger.info(res.content)
-    es = Elasticsearch([{'host':'atlas-kibana.mwt2.org', 'port':9200}])
+    #res = requests.get('http://atlas-kibana.mwt2.org:9200')
+    #logger.info(res.content)
+    #es = Elasticsearch([{'host':'atlas-kibana.mwt2.org', 'port':9200}])
 
 AllTransfers={}
 AllServers={}
 AllUsers={}
 
-def addRecord(sid,userID,fileClose,timestamp):
-    rec={
-        '_type': 'detailed'
-    }
+connect_config = ConfigParser.ConfigParser()
+connect_config.read('connection.conf')
+
+
+def CreateRabbitConnection():
+    global channel
+    parameters = pika.URLParameters(connect_config.get('AMQP', 'url'))
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+
+
+
+def addRecord(sid,userID,fileClose,timestamp,addr):
+    rec={}
     rec['timestamp']=timestamp*1000
     
+    rec['server_hostname'] = socket.gethostbyaddr(addr)[0]
+    rec['server_ip'] = addr
     if sid in AllServers:
         s = AllServers[sid]
         rec['serverID'] = sid
         rec['server'] = s.addr
         rec['site'] = s.site
     else:
+        rec['server'] = addr
         logger.warning('server still not identified: %i',sid) 
         
     try:
@@ -65,28 +83,50 @@ def addRecord(sid,userID,fileClose,timestamp):
     except KeyError:
         logger.error( '%suser %i missing.%s',decoding.bcolors.WARNING, userID, decoding.bcolors.ENDC)
         # print decoding.bcolors.WARNING + 'user ' + str(userID) + ' missing.' + decoding.bcolors.ENDC
-
-    f = AllTransfers[sid][userID][fileClose.fileID]
+    transfer_key = str(sid) + "." + str(fileClose.fileID)
+    f = AllTransfers[transfer_key][1]
     rec['filename'] = f.fileName
     rec['filesize'] = f.fileSize
     rec['read']     = fileClose.read
     rec['readv']    = fileClose.readv
     rec['write']    = fileClose.write
+
+    # Set the file-domains
     
-    d = datetime.now()
-    mont=str(d.month)
-    if len(mont)<2: mont = '0' + mont
-    ind="sc_xrd_detailed-"+str(d.year) + "." + mont
-    rec['_index']=ind
+    rec['dirname1'] = "/".join(f.fileName.split('/', 2)[:2])
+    rec['dirname2'] = "/".join(f.fileName.split('/', 3)[:3])
+    if f.fileName.startswith('/user'):
+        rec['logical_dirname'] = rec['dirname2']
+    elif f.fileName.startswith('/pnfs/fnal.gov/usr'):
+        rec['logical_dirname'] = "/".join(f.fileName.split('/')[:5])
+        
+    print rec
+    try:
+    	channel.basic_publish(connect_config.get('AMQP', 'exchange'),
+                      "file-close",
+                      json.dumps(rec),
+                      pika.BasicProperties(content_type='application/json',
+                                           delivery_mode=1))
+    except Exception as e:
+        logger.exception('Error while sending rabbitmq message')
+        CreateRabbitConnection()
+        channel.basic_publish(connect_config.get('AMQP', 'exchange'),
+                      "file-close",
+                      json.dumps(rec),
+                      pika.BasicProperties(content_type='application/json',
+                                           delivery_mode=1))
+    
+    
     return rec
     
 def eventCreator():
     
     aLotOfData=[]
+    last_flush = time.time()
     while(True):
         [d,addr]=q.get()
         
-        # print "\nByte Length of Message :", len(d)
+        print "\nByte Length of Message :", len(d)
         
         h=decoding.header._make(struct.unpack("!cBHI",d[:8])) # XrdXrootdMonHeader
         
@@ -99,13 +139,25 @@ def eventCreator():
         #     print h
         # else:
         #     print '*',
-                        
+        print h
         logger.debug(h)    
         
         d=d[8:]
+        # Summarize current datastructure
+        #num_servers = len(AllTransfers)
+        #num_users = 0
+        #num_files = 0
+        #for sid in AllTransfers:
+        #    num_users += len(AllTransfers[sid])
+        #    for user in AllTransfers[sid]:
+        #        num_files += len(AllTransfers[sid][user])
+
+        #print "Servers: {0}, Users: {1}, Files: {2}".format(num_servers, num_users, num_files)
+
         
         
         if (h.code=='f'):
+            logger.debug("Got fstream object")
             TimeRecord=decoding.MonFile(d) # first one is always TOD
             logger.debug(TimeRecord)
             d=d[TimeRecord.recSize:]
@@ -115,6 +167,7 @@ def eventCreator():
                 d=d[hd.recSize:]
                 
                 logger.debug('%i %s', i, hd)
+                print i, hd
                 # if debug: print i, hd
                 
                 if isinstance(hd, decoding.fileDisc):
@@ -126,31 +179,47 @@ def eventCreator():
                         #print decoding.bcolors.WARNING + 'User that disconnected was unknown.' + decoding.bcolors.ENDC
                 
                 elif isinstance(hd, decoding.fileOpen):
-                    if sid not in AllTransfers:
-                        AllTransfers[sid]={}
-                    if hd.userID not in AllTransfers[sid]:
-                        AllTransfers[sid][hd.userID]={}
-                    AllTransfers[sid][hd.userID][hd.fileID]=hd
+                    #if sid not in AllTransfers:
+                    #    AllTransfers[sid]={}
+                    #if hd.userID not in AllTransfers[sid]:
+                    #    AllTransfers[sid][hd.userID]={}
+                    
+                    transfer_key = str(sid) + "." + str(hd.fileID)
+                    AllTransfers[transfer_key]=((time.time(), addr), hd)
                     
                 elif isinstance(hd, decoding.fileClose):
                     logger.debug('%i %s', i, hd)
-                    if sid in AllTransfers:
-                        found=0
-                        for u in AllTransfers[sid]:
-                            if hd.fileID in AllTransfers[sid][u]:
-                                found=1
-                                rec = addRecord(sid, u, hd, TimeRecord.tEnd)
-                                aLotOfData.append( rec  )
-                                del AllTransfers[sid][u][hd.fileID]
-                                if len(AllTransfers[sid][u])==0: del AllTransfers[sid][u]
-                                break
-                        if not found:
-                            logger.error("%sfile to close not found.%s", decoding.bcolors.WARNING, decoding.bcolors.ENDC)
-                            # print decoding.bcolors.WARNING + "file to close not found." + decoding.bcolors.ENDC
+                    transfer_key = str(sid) + "." + str(hd.fileID)
+                    if transfer_key in AllTransfers:
+                        u = AllTransfers[transfer_key][1].userID
+                        rec = addRecord(sid, u, hd, TimeRecord.tEnd, addr)
+                        print rec
+                        #aLotOfData.append( rec  )
+                        del AllTransfers[transfer_key]
+                    #if sid in AllTransfers:
+                    #    found=0
+                    #    for u in AllTransfers[sid]:
+                    #        if hd.fileID in AllTransfers[sid][u]:
+                    #            found=1
+                    #            rec = addRecord(sid, u, hd, TimeRecord.tEnd, addr)
+                    #            print rec
+                    #            aLotOfData.append( rec  )
+                    #            del AllTransfers[sid][u][hd.fileID]
+                    #            if len(AllTransfers[sid][u])==0: del AllTransfers[sid][u]
+                    #            break
                     else:
-                        logger.error("%sfile closed on server that's not found%s", decoding.bcolors.WARNING, decoding.bcolors.ENDC)
-                        # print decoding.bcolors.WARNING + "file closed on server that's not found" + decoding.bcolors.ENDC
-                        AllTransfers[sid]={}
+                        logger.error("%sfile to close not found. fileID: %i, serverID: %i%s", decoding.bcolors.WARNING, hd.fileID, sid, decoding.bcolors.ENDC)
+                elif isinstance(hd, decoding.fileXfr):
+                    transfer_key = str(sid) + "." + str(hd.fileID)
+                    if transfer_key in AllTransfers:
+                        cur_value = AllTransfers[transfer_key]
+                        AllTransfers[transfer_key] = ((time.time(), cur_value[0][1]), cur_value[1], hd)
+                        print "Adding xfrInfo" 
+                        # print decoding.bcolors.WARNING + "file to close not found." + decoding.bcolors.ENDC
+                    #else:
+                    #    logger.error("%sfile closed on server that's not found%s", decoding.bcolors.WARNING, decoding.bcolors.ENDC)
+                    #    # print decoding.bcolors.WARNING + "file closed on server that's not found" + decoding.bcolors.ENDC
+                    #    AllTransfers[sid]={}
                                 
                 
         elif (h.code=='r'):
@@ -208,18 +277,37 @@ def eventCreator():
                     
             elif (h.code=='x'):
                 xfrInfo=decoding.xfrInfo(rest)
+                #transfer_key = str(sid) + "." + str(xfrInfo.fileID)
+                #if transfer_key in AllTransfers:
+                #    cur_value = AllTransfers[transfer_key]
+                #    AllTransfers[transfer_key] = (time.time(), cur_value[1], xfrInfo)
+                #    print "Adding xfrInfo"
+                
                 # print xfrInfo
         
         q.task_done()
         
+        # Check if we have to flush the AllTransfer
+        now_time = time.time()
+        if (now_time - last_flush) > (60*5):
+            for key in AllTransfers.keys():
+                cur_value = AllTransfers[key]
+                if (now_time - cur_value[0][0]) > (60*60*6):
+                    if len(cur_value) == 3:
+                        (sid, u) = key.split(".")
+                        sid = int(sid)
+                        u = int(u)
+                        addr = cur_value[0][1]
+                        rec = addRecord(sid, u, cur_value[2], now_time, addr)
+                    del AllTransfers[key]
+ 
         if q.qsize()>200:
-            logger.error('Some problem in sending data to ES. Trying to reconnect.')
-            RefreshConnection()
+            logger.error('QSize is large: {0}'.format(q.qsize()))
             
         if len(aLotOfData)>50:
             try:
-                res = helpers.bulk(es, aLotOfData, raise_on_exception=True)
-                logger.info('%s  inserted: %i  errors: %s',threading.current_thread().name, res[0], str(res[1]))
+                #res = helpers.bulk(es, aLotOfData, raise_on_exception=True)
+                #logger.info('%s  inserted: %i  errors: %s',threading.current_thread().name, res[0], str(res[1]))
                 aLotOfData=[]
             except es_exceptions.ConnectionError as e:
                 logger.error('ConnectionError %s', e)
@@ -233,15 +321,18 @@ def eventCreator():
                     if errcount>5: break
                     logger.error('%s',i)
             except:
-                logger.error('Something serious happened.')
+                logger.exception('Something serious happened.')
                 e = sys.exc_info()[0]
                 logger.error(e)
     logger.error('SOMEHOW WENT OUT OF THE LOOP!')
 
 es = None
 lastReconnectionTime=0
-while (not es):
-    RefreshConnection()
+#while (not es):
+#    RefreshConnection()
+
+
+CreateRabbitConnection()
 
 q=Queue.Queue()
 #start eventCreator threads
@@ -249,27 +340,34 @@ for i in range(1):
      t = Thread(target=eventCreator)
      t.daemon = True
      t.start()
-     
+    
+
+debug_file = open('stuff.err', 'a')
 nMessages=0
 debug=False
 while (True):
     message, addr = sock.recvfrom(65536) # buffer size is 1024 bytes
-    # print ("received message:", message, "from:", addr)
+    if len(message) > 50000:
+        debug_file.write("Size of packet is large: {0}\n".format(len(message)))
+
+    print addr
+    #print ("received message:", message, "from:", addr)
     q.put([message,addr[0]])
     nMessages+=1
-    if (nMessages%100==0):
-        logger.info("messages received: %i   qsize: %i", nMessages, q.qsize())
-        if debug:
-            print "All Servers:", AllServers
-            print "All Users:"
-            for sid in AllUsers:
-                print sid, len(AllUsers[sid]) 
-                for uid in AllUsers[sid]:  # enabling this can crash it if size change during iteration
-                    print uid, AllUsers[sid][uid]
-            print "All Transfers:"
-            for sid in AllTransfers:
-                print sid
-                for uid in AllTransfers[sid]:
-                    print uid
-                    for fid in AllTransfers[sid][uid]:
-                        print fid, AllTransfers[sid][uid][fid]
+    #if (nMessages%100==0):
+    #    logger.info("messages received: %i   qsize: %i", nMessages, q.qsize())
+    #    if debug:
+    #        print "All Servers:", AllServers
+    #        print "All Users:"
+    #        for sid in AllUsers:
+    #            print sid, len(AllUsers[sid]) 
+    #            for uid in AllUsers[sid]:  # enabling this can crash it if size change during iteration
+    #                print uid, AllUsers[sid][uid]
+    #        print "All Transfers:"
+    #        for sid in AllTransfers:
+    #            print sid
+    #            for uid in AllTransfers[sid]:
+    #                print uid
+    #                for fid in AllTransfers[sid][uid]:
+    #                    print fid, AllTransfers[sid][uid][fid]
+
