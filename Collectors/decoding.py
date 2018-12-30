002e9c4925a1976425f81a1149efeae7f26b5b3e
Brian Bockelman <bbockelm@cse.unl.edu>
2018-12-29 22:13:16 -0600

    Add support for python3 in decoding.py

--- Collectors/decoding.py
+++ Collectors/decoding.py
@@ -7,8 +7,8 @@ import sys
 header = namedtuple("header", ["code", "pseq", "plen", "server_start"])
 mapheader = namedtuple("mapheader", ["dictID", "info"])
 
-userid   = namedtuple("userid", ["username", "pid", "sid", "host"])
-authinfo = namedtuple("authinfo", ["ap", "dn", "hn", "on", "rn", "gn", "info", "inetv"])
+userid   = namedtuple("userid", ["protocol", "username", "pid", "sid", "host"])
+authinfo = namedtuple("authinfo", ["ap", "dn", "hn", "on", "rn", "gn", "info", 'execname', 'moninfo', "inetv"])
 srvinfo  = namedtuple("srvinfo", ["program", "version", "instance", "port", "site", "addr"])
 prginfo  = namedtuple("prginfo", ["xfn", "tod", "sz", "at", "ct", "mt", "fn"])
 xfrinfo  = namedtuple("xfrinfo", ["lfn", "tod", "sz", "tm", "op", "rc", "pd"])
@@ -23,13 +23,13 @@ ops       = namedtuple("ops", ["read", "readv", "write", "rsMin", "rsMax", "rseg
 
 def userInfo(message):
     c = message
-    if '/' in message:
-        prot, c = message.split('/', 1)
-    hind = c.rfind('@')
+    if b'/' in message:
+        prot, c = message.split(b'/', 1)
+    hind = c.rfind(b'@')
     host = c[hind + 1:]
     c = c[:hind]
-    c, sid = c.split(':', 1)
-    hind = c.rfind('.')
+    c, sid = c.split(b':', 1)
+    hind = c.rfind(b'.')
     pid = c[hind + 1:]
     user = c[:hind]
     pi = 0
@@ -39,68 +39,72 @@ def userInfo(message):
         si = int(sid)
     except ValueError:
         print("serious value error: ", pid, sid, "message was:", message)
-    return userid(user, pi, si, host)
+    return userid(prot, user, pi, si, host)
 
 
 def authorizationInfo(message):
-    r = message.split('&')
-    ap = dn = hn = on = rn = gn = m = inetv = ''
+    r = message.split(b'&')
+    ap = dn = hn = on = rn = gn = info = execname = moninfo = inetv = b''
     for i in r:
-        kv = i.split('=')
+        kv = i.split(b'=', 1)
         if len(kv) == 2:
-            if kv[0] == 'p':
+            if kv[0] == b'p':
                 ap = kv[1]
-            elif kv[0] == 'n':
+            elif kv[0] == b'n':
                 dn = kv[1]
-            elif kv[0] == 'h':
+            elif kv[0] == b'h':
                 hn = kv[1]
-            elif kv[0] == 'o':
+            elif kv[0] == b'o':
                 on = kv[1]
-            elif kv[0] == 'r':
+            elif kv[0] == b'r':
                 rn = kv[1]
-            elif kv[0] == 'g':
+            elif kv[0] == b'g':
                 gn = kv[1]
-            elif kv[0] == 'm':
-                m = kv[1]
-            elif kv[0] == 'I':
+            elif kv[0] == b'm':
+                info = kv[1]
+            elif kv[0] == b'I':
                 inetv = kv[1]
-    return authinfo(ap, dn, hn, on, rn, gn, m, inetv)
+            elif kv[0] == b'x':
+                execname = kv[1]
+            elif kv[0] == b'y':
+                moninfo = kv[1]
+    return authinfo(ap, dn, hn, on, rn, gn, info, execname, moninfo, inetv)
 
 
 def serverInfo(message, addr):
-    r = message.split('&')
-    pgm = r[1].split('=')[1]
-    ver = r[2].split('=')[1]
-    inst = r[3].split('=')[1]
-    port = r[4].split('=')[1]
-    site = r[5].split('=')[1]
+    r = message.split(b'&')
+    pgm = r[1].split(b'=')[1]
+    ver = r[2].split(b'=')[1]
+    inst = r[3].split(b'=')[1]
+    port = r[4].split(b'=')[1]
+    site = r[5].split(b'=')[1]
     return srvinfo(pgm, ver, inst, port, site, addr)
 
 
 def purgeInfo(message):
-    xfn, rest = message.split('\n')
-    r = rest.split("&")
-    tod = r[1].split('=')[1]
-    sz = r[2].split('=')[1]
-    at = r[3].split('=')[1]
-    ct = r[4].split('=')[1]
-    mt = r[5].split('=')[1]
-    fn = r[6].split('=')[1]
+    xfn, rest = message.split(b'\n')
+    r = rest.split(b"&")
+    tod = r[1].split(b'=')[1]
+    sz = r[2].split(b'=')[1]
+    at = r[3].split(b'=')[1]
+    ct = r[4].split(b'=')[1]
+    mt = r[5].split(b'=')[1]
+    fn = r[6].split(b'=')[1]
     return prginfo(xfn, tod, sz, at, ct, mt, fn)
 
 
 def xfrInfo(message):
-    lfn, rest = message.split('\n')
-    r = rest.split("&")
-    tod = r[1].split('=')[1]
-    sz = r[2].split('=')[1]
-    tm = r[3].split('=')[1]
-    op = r[4].split('=')[1]
-    rc = r[5].split('=')[1]
+    lfn, rest = message.split(b'\n')
+    r = rest.split(b"&")
+    tod = r[1].split(b'=')[1]
+    sz = r[2].split(b'=')[1]
+    tm = r[3].split(b'=')[1]
+    op = r[4].split(b'=')[1]
+    rc = r[5].split(b'=')[1]
     if len(r) == 7:
-        pd = r[6].split('=')[1]
+        pd = r[6].split(b'=')[1]
     else:
-        pd = ''
+        pd = b''
     return xfrinfo([lfn, tod, sz, tm, op, rc, pd])
 
 
@@ -119,10 +123,10 @@ def MonFile(d):
         fO = struct.unpack("!BBHIQ", d[:16])
         if up[1] == 1:
             userId = struct.unpack("!I", d[16:20])[0]
-            fileName = struct.unpack("!" + str(up[2] - 20) + "s", d[20:up[2]])[0].rstrip('\0')
+            fileName = struct.unpack("!" + str(up[2] - 20) + "s", d[20:up[2]])[0].rstrip(b'\0')
         else:
             userId = 0
-            fileName = ''
+            fileName = b''
         return fileOpen._make(fO + (userId, fileName))
     elif up[0] == 2:  # isTime
         if up[2] == 16:  # this and next 3 lines can be removed after the fixed montiring stream is deployed.
