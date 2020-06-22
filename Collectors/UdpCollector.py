c60eb44257aefa3abb10786dcb2d28f142679b4d
Derek Weitzel <djw8605@gmail.com>
2020-06-22 10:55:46 -0500

    Fix naming of metric

--- Collectors/UdpCollector.py
+++ Collectors/UdpCollector.py
@@ -246,7 +246,7 @@ class UdpCollector(object):
             elif metrics_message['type'] == "packets":
                 packets.inc(metrics_message['count'])
             elif metrics_message['type'] == "reordered packets":
-                packets.inc(metrics_message['count'])
+                reorder_counter.inc(metrics_message['count'])
 
 
 
