c83f2f650aac00944acdf1c6543ab058ce65a45f
Derek Weitzel <djw8605@gmail.com>
2023-09-05 20:35:44 -0500

    Update Collectors/UdpCollector.py
    Co-authored-by: jthiltges <john@thiltges.com>

--- Collectors/UdpCollector.py
+++ Collectors/UdpCollector.py
@@ -276,7 +276,7 @@ class UdpCollector(object):
             # High water mark for the queue
             if len(message_q.qsize() > config.get('Worker', 'high_water_mark', fallback=100)):
                 while message_q.qsize() > config.get('Worker', 'low_water_mark', fallback=10):
-                    logging.error("Sleeping for 1 second to allow the queue to drain")
+                    logging.error("Sleeping for 1 second to allow the queue to drain: {} pending".format(message_q.qsize()))
                     time.sleep(1)
             message_q.put([message, addr[0], addr[1]])
             channel.basic_ack(method.delivery_tag)
